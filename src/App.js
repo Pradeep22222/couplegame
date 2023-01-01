e5476ddd15f613d6020838038aaf3241757c75b5
PK <pradeepdhital124@gmail.com>
2023-01-01 13:12:03 +1100

    homepage partially done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
-
+import "bootstrap/dist/css/bootstrap.min.css";
+// import {BrowserRouter, Routes, Route} from "react-router-dom"
+import { Home } from './components/Home';
 function App() {
   return (
-    <div className="App">
-      
+    <div className="app">
+      <Home></Home>
     </div>
   );
 }
---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const Home = () => {
+  return (
+    <div className="homepage">
+      <i class="fa-solid fa-heart hearts"></i>
+      <i class="fa-solid fa-heart-pulse hearts"></i>
+      <h1>Welcome to the couple game</h1>
+      <i class="fa-solid fa-heart-pulse hearts"></i>
+      <i class="fa-solid fa-heart hearts"></i>
+    </div>
+  );
+}
