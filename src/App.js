f842ec6279f302d8988efaee3d676e21a17eac3e
PK <pradeepdhital124@gmail.com>
2023-01-02 11:08:33 +1100

    done upto facebook title cololring

--- src/App.js
+++ src/App.js
@@ -6,12 +6,14 @@ import Container from "react-bootstrap/Container";
 // import {BrowserRouter, Routes, Route} from "react-router-dom"
 import { Home } from "./components/Home";
 import { PartnerConnection } from "./components/PartnerConnection";
+import { Fblogin } from "./components/Fblogin";
 function App() {
   return (
     <div className="app">
       <Container>
         <Home></Home>
         <PartnerConnection></PartnerConnection>
+        <Fblogin></Fblogin>
         <img src={hearts} className="hearts"></img>
       </Container>
     </div>
---
+++ src/components/Fblogin.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Row from "react-bootstrap/Row";
+
+export const Fblogin = () => {
+  return (
+    <div>
+      <Row className="text-center">
+        <div className="facebook_heading">Facebook</div>
+      </Row>
+    </div>
+  );
+}
