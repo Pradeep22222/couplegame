d6b6115b9abf1a064017a7e4f494dc63555754d9
PK <pradeepdhital124@gmail.com>
2023-01-02 13:32:33 +1100

    input field colors to be set

--- src/components/Fblogin.js
+++ src/components/Fblogin.js
@@ -1,4 +1,6 @@
-import React from 'react'
+import React from "react";
+import Col from "react-bootstrap/Col";
+import Form from "react-bootstrap/Form";
 import Row from "react-bootstrap/Row";
 
 export const Fblogin = () => {
@@ -7,6 +9,26 @@ export const Fblogin = () => {
       <Row className="text-center">
         <div className="facebook_heading">Facebook</div>
       </Row>
+      <Form>
+        <Row className="mb-3">
+          <Col></Col>
+          <Col>
+            <Form.Control
+              placeholder="First name"
+              className="facebook_inputs"
+              type="text"
+            />
+          </Col>
+          <Col></Col>
+        </Row>
+        <Row>
+          <Col></Col>
+          <Col>
+            <Form.Control placeholder="Last name" className="facebook_inputs" />
+          </Col>
+          <Col></Col>
+        </Row>
+      </Form>
     </div>
   );
-}
+};
