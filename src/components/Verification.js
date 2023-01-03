9977d94adb397a6b5320107b4688a77a9fbce15e
PK <pradeepdhital124@gmail.com>
2023-01-03 11:32:40 +1100

    now left to apply single page action

--- src/components/Verification.js
+++ src/components/Verification.js
@@ -2,6 +2,7 @@ import React from "react";
 import Form from "react-bootstrap/Form";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+import Button from "react-bootstrap/Button";
 
 export const Verification = () => {
   return (
@@ -11,7 +12,7 @@ export const Verification = () => {
         <span className="mobile_icon"></span>
         <i className="fa-solid fa-mobile mobile_icon__icon"></i>
       </div>
-      <Row>Enter Code</Row>
+      <div className="text-center">Enter Code</div>
       <Row>
         Enter 4-6 digit code we sent to your registered mobile number to log
         into facebook. You might have to wait for upto 2 minutes.
@@ -20,17 +21,22 @@ export const Verification = () => {
         <Row>
           <Col></Col>
           <Col>
-            <Form.Control placeholder="First name" />
+            <Form.Control
+              placeholder="confirmation code"
+              className="text-center"
+            />
           </Col>
           <Col></Col>
         </Row>
       </Form>
       <Row>
-        <span>
-          <i class="fa-solid fa-arrow-rotate-right"></i>
-        </span>
-        <div>Resend Code</div>
-      </Row>
+        <a className="text-center link_underline">
+          <i class="fa-solid fa-arrow-rotate-right"></i>Resend Code
+        </a>
+          </Row>
+          <Row>
+              <Button>Verify Account</Button>
+          </Row>
     </div>
   );
 };
